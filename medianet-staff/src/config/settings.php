7464f78903b77eb22782ca1ff518bce4f3898a53
apinot <antoine.pinot1@gmail.com>
2019-11-04 17:15:26 +0100

    twig settings

--- medianet-staff/src/config/settings.php
+++ medianet-staff/src/config/settings.php
@@ -1,6 +1,19 @@
 <?php
+
+use \Slim\Views\Twig;
+
 return [
-        'settings' => [
-            'displayErrorDetails' => true,
-        ],
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+    'view' => function ($c) {
+        $view = new Twig('../views', [
+            'debug' => true
+            ]);
+            
+            // Instantiate and add Slim specific extension
+            $router = $c->get('router');
+            $uri = Uri::createFromEnvironment(new Environment($_SERVER));
+            $view->addExtension(new TwigExtension($router, $uri));
+        },
     ];
