090ffe0a1dde224625795078ddac32c34bf41e34
apinot <antoine.pinot1@gmail.com>
2019-11-04 16:27:22 +0100

    update bdd

---
+++ documents/medianet.sql
@@ -0,0 +1,186 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost:3306
+-- Généré le :  Lun 04 Novembre 2019 à 16:24
+-- Version du serveur :  5.7.27-0ubuntu0.18.04.1
+-- Version de PHP :  7.2.24-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `medianet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cds`
+--
+
+CREATE TABLE `cds` (
+  `id` int(11) NOT NULL,
+  `artistes` varchar(255) NOT NULL,
+  `maison_disque` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `documents`
+--
+
+CREATE TABLE `documents` (
+  `id` int(11) NOT NULL,
+  `document_id` int(11) NOT NULL,
+  `document_type` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `resume` text NOT NULL,
+  `genre` varchar(255) NOT NULL,
+  `disponible` tinyint(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `dvds`
+--
+
+CREATE TABLE `dvds` (
+  `id` int(11) NOT NULL,
+  `acteurs` varchar(255) NOT NULL,
+  `duree` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `emprunts`
+--
+
+CREATE TABLE `emprunts` (
+  `id` int(11) NOT NULL,
+  `document_id` int(11) NOT NULL,
+  `dateEmprunt` timestamp NULL DEFAULT NULL,
+  `dateRetour` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `livres`
+--
+
+CREATE TABLE `livres` (
+  `id` int(11) NOT NULL,
+  `auteur` varchar(255) NOT NULL,
+  `edition` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `mdp` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `adhesion` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `cds`
+--
+ALTER TABLE `cds`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `documents`
+--
+ALTER TABLE `documents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `dvds`
+--
+ALTER TABLE `dvds`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `emprunts`
+--
+ALTER TABLE `emprunts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `livres`
+--
+ALTER TABLE `livres`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `cds`
+--
+ALTER TABLE `cds`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `documents`
+--
+ALTER TABLE `documents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `dvds`
+--
+ALTER TABLE `dvds`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `emprunts`
+--
+ALTER TABLE `emprunts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `livres`
+--
+ALTER TABLE `livres`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
