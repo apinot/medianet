f188acb22005e8788acf3e2706ca017cd47aed19
felixleo22 <leofelixoff@outlook.fr>
2019-11-05 09:56:51 +0100

    formulaire de connexion + methode pour se connecter

--- medianet-users/src/public/index.php
+++ medianet-users/src/public/index.php
@@ -3,6 +3,7 @@ require_once('../../vendor/autoload.php');
 
 //controllers
 use medianet\controllers\ControllerHome;
+use medianet\controllers\ControllerUser;
 
 //database connection with Eloquent
 $capsule = new \Illuminate\Database\Capsule\Manager;
@@ -20,6 +21,7 @@ $app = new \Slim\App($container);
 $app->get('/', ControllerHome::class.':index')->setName('home');
 
 
-$app->get('/connexion', UserController::class.':afficherFomulaireConnexion')->setName('formConnexion');
+$app->get('/connexion', ControllerUser::class.':afficherFomulaireConnexion')->setName('formConnexion');
+$app->post('/connexion', ControllerUser::class.':connecter')->setName('execConnexion');
 
 $app->run();
