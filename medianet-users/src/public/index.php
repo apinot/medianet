d733ed46bda8f89b16a4e0687fa82bc9b435a1d3
simon <simon.dal-ponte8@etu.univ-lorraine.fr>
2019-11-05 10:11:40 +0100

    resolution de conflit

--- medianet-users/src/public/index.php
+++ medianet-users/src/public/index.php
@@ -3,7 +3,7 @@ require_once('../../vendor/autoload.php');
 
 //controllers
 use medianet\controllers\ControllerHome;
-use medianet\controllers\ControllerUser;
+//use medianet\controllers\ControllerUser;
 
 //database connection with Eloquent
 $capsule = new \Illuminate\Database\Capsule\Manager;
@@ -18,34 +18,13 @@ $app = new \Slim\App($container);
 
 /** Routes */
 //affichage de la page d'accueil
-<<<<<<< HEAD
-
-
-//Installation de twig
-$container['view'] = function($container) {
-    $view = new \Slim\Views\Twig('../views', []);
-    //Instantiate and add Slim specific extension
-    $router = $container->get('router');
-    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
-    $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
-
-    return $view;
-};
-
 
 
 $app->get('/', \medianet\controllers\IndexUserController::class.':listMedia')->setName('acceuil');
 
-=======
-$app->get('/', ControllerHome::class.':index')->setName('home');
 
-
-<<<<<<< HEAD
-$app->get('/connexion', UserController::class.':afficherFomulaireConnexion')->setName('formConnexion');
->>>>>>> 51c38e42f08ffb7cfcecb36d782785aa9c0adbfe
-=======
-$app->get('/connexion', ControllerUser::class.':afficherFomulaireConnexion')->setName('formConnexion');
-$app->post('/connexion', ControllerUser::class.':connecter')->setName('execConnexion');
->>>>>>> f188acb22005e8788acf3e2706ca017cd47aed19
+//$app->get('/connexion', UserController::class.':afficherFomulaireConnexion')->setName('formConnexion');
+//$app->get('/connexion', ControllerUser::class.':afficherFomulaireConnexion')->setName('formConnexion');
+//$app->post('/connexion', ControllerUser::class.':connecter')->setName('execConnexion');
 
 $app->run();
