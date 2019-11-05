6fe0178f0d014a14dcde1e073ec31172193cb21c
simon <simon.dal-ponte8@etu.univ-lorraine.fr>
2019-11-05 11:54:50 +0100

    ajout des card et la liste des media

--- medianet-users/src/public/index.php
+++ medianet-users/src/public/index.php
@@ -16,20 +16,6 @@ $settings = require_once "../config/settings.php";
 $container = new \Slim\Container($settings);
 $app = new \Slim\App($container);
 
-/** Routes */
-//affichage de la page d'accueil
-
-//Installation de twig
-$container['view'] = function($container) {
-    $view = new \Slim\Views\Twig('../views', []);
-    //Instantiate and add Slim specific extension
-    $router = $container->get('router');
-    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
-    $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
-
-    return $view;
-};
-
 
 $app->get('/', \medianet\controllers\IndexUserController::class.':listMedia')->setName('acceuil');
 
@@ -37,7 +23,7 @@ $app->get('/', \medianet\controllers\IndexUserController::class.':listMedia')->s
 //$app->get('/connexion', UserController::class.':afficherFomulaireConnexion')->setName('formConnexion');
 //$app->get('/connexion', ControllerUser::class.':afficherFomulaireConnexion')->setName('formConnexion');
 //$app->post('/connexion', ControllerUser::class.':connecter')->setName('execConnexion');
-$app->get('/', ControllerHome::class.':index')->setName('home');
+//$app->get('/', ControllerHome::class.':index')->setName('home');
 
 //$app->get('/connexion', UserController::class.':afficherFomulaireConnexion')->setName('formConnexion');
 //$app->post('/connexion', ControllerUser::class.':connecter')->setName('execConnexion');
