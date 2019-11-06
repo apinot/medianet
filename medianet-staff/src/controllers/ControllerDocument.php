f492829bf5f09b764e0a87231edee074e9c8b6c6
simon <simon.dal-ponte8@etu.univ-lorraine.fr>
2019-11-06 13:57:43 +0100

    ajout de la fonctionnalité supprimer/editer des users

---
+++ medianet-staff/src/controllers/ControllerDocument.php
@@ -0,0 +1,68 @@
+<?php
+namespace medianet\controllers;
+
+use medianet\models\Document;
+use medianet\models\Livre;
+use medianet\models\CD;
+use Slim\Exception\NotFoundException;
+
+class ControllerDocument extends Controller {
+    
+    public function showDocument($request, $response, $args) {
+        $idDoc = Utils::sanitize($args['id']);
+        $doc = Document::find($idDoc);
+        if($doc == null){
+            throw new NotFoundException($request, $response);
+        }
+        
+        $type = $doc->documentable_type;
+        
+        $twigUrl = null;
+        switch ($type) {
+            case Livre::class:
+            $twigUrl  = 'documents/livre.html.twig';
+            break;
+            case CD::class:
+            $twigUrl  = 'documents/cd.html.twig';
+            break;
+            case DVD::class:
+            $twigUrl = 'documents/dvd.html.twig';
+            break;
+        }
+        
+        if($twigUrl == null){
+            throw new NotFoundException($request, $response);
+        }
+        
+        return $this->render($response, $twigUrl, ['document' => $doc]);
+    }
+    
+    public function filter($request, $response, $args)
+    {
+        $queries = [];
+        $queries['motcle'] = Utils::getFilteredGet($request, 'keyword');
+        $queries['type'] = Utils::getFilteredGet($request, 'doctype');
+        $queries['genre'] = Utils::getFilteredGet($request, 'kind');
+
+        $motcle = $queries['motcle'];
+        $genre = $queries['genre'];
+        $type = $queries['type'];
+
+        $req = Document::where(function ($query) use ($motcle) {
+            $query->where('nom', 'like', '%'.$motcle.'%')
+            ->orWhere('resume', 'like', '%'.$motcle.'%');
+        });
+        
+        if($genre !== null && $genre != "") {
+            $req->where('genre', 'like', '%'.$genre.'%');
+        }
+        
+        if($type !== null && $type !== "" && $type !== 'all') {
+            $req->where('documentable_type', 'like', '%'.$type);
+        }
+        
+        $medias = $req->get();
+        
+        return $this->view->render($response, 'index.html.twig', ['medias' => $medias, 'query' => $queries]);
+    }
+}
--- medianet-staff/src/public/index.php
+++ medianet-staff/src/public/index.php
@@ -3,6 +3,7 @@ require_once('../../vendor/autoload.php');
 
 //controllers
 use medianet\controllers\ControllerStaff;
+use medianet\controllers\ControllerUser;
 
 //database connection with Eloquent
 $capsule = new \Illuminate\Database\Capsule\Manager;
@@ -19,6 +20,17 @@ $app = new \Slim\App($container);
 //affichage de la page d'accueil
 $app->get('/', ControllerStaff::class.':accueil')->setName('home');
 
+$app->get('/membres', ControllerUser::class.':membersList')->setName('membres');
+$app->get('/details/{id}', ControllerUser::class.':detailsMembers')->setName('details_membre');
+
+$app->get('/modifier/{id}', ControllerUser::class.':showUser')->setname('formUpdateUser');
+$app->post('/modifier/{id}', ControllerUser::class.':updateUser')->setName('execUpdateUser');
+
+$app->get('/delete/{id}', ControllerUser::class.':delete')->setName('delete');
+
+$app->get('/pwd', ControllerUser::class.':pwdPage')->setName('updatePwd');
+$app->post('/pwd', ControllerUSer::class.':changePwd')->setName('lookPwd');
+$app->get('/document/{id}', ControllerDocument::class.':showDocument')->setName('showDocument');
 //emprunts
 $app->get('/emprunt', ControllerStaff::class.':pageEmprunt')->setName('emprunter');
 $app->post('/emprunt', ControllerStaff::class.':which')->setName('lookEmprunt');
