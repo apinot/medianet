bb9ac9eedd2ead0ca87937e05ea64c6539a470e1
simon <simon.dal-ponte8@etu.univ-lorraine.fr>
2019-11-08 09:33:55 +0100

    avant le pull

--- medianet-staff/src/public/js/flash.js
+++ medianet-staff/src/public/js/flash.js
@@ -5,18 +5,18 @@ let flash_info = document.querySelector('.info');
 if(flash_sucess!=null){
     setTimeout(function () {
         $("[class=success]").hide();
-    },1000)
+    },9999999999999)
 }
 
 if(flash_danger!=null){
     setTimeout(function () {
         $("[class=danger]").hide();
-    },1000)
+    },9999999999999)
 }
 
 if(flash_info!=null){
     setTimeout(function () {
         $("[class=info]").hide();
-    },1000)
+    },9999999999999)
 }
 
